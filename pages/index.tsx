248df6582799f79fdcf34d773bbe5fe69604715b
shin-yujin-mac <in1006sh@gmail.com>
2023-09-16 16:56:16 +0900

    #2.1 Fetching Data  임시 commit;

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,32 @@
 import Seo from "../components/Seo";
+import {useEffect, useState} from "react";
+
+const API_KEY = "";
 
 export default function Home() {
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        (async () => {
+            const { res } = await (
+                await fetch(
+                `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+                )
+            ).json();
+            console.log(res)
+            console.log(typeof res)
+            setMovies(res);
+        })();
+    }, []);
     return (
         <div>
             <Seo title = "Home"/>
-            <h1 className="active">Hello</h1>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map((movie) => (
+                <div key={movie.id}>
+                    <h4>{movie.original_title}</h4>
+                </div>
+
+            ))}
             <style jsx>{`
               a {
               color: white;
